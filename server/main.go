311f622b1d2870c697f4bc8e210cb576c481527f
Vicente Reyes <vicente@vichoreyes.cl>
2024-05-01 22:08:39 -0400

    improve server

--- server/main.go
+++ server/main.go
@@ -6,6 +6,6 @@ import (
 )
 
 func main() {
-	fmt.Println("Starting server on :8080")
+	fmt.Println("Starting server on http://localhost:8080/")
 	http.ListenAndServe(":8080", http.FileServer(http.Dir(".")))
 }
