a845c2caefce2113b29f66c5bd871c644562aae1
Vicente Reyes <vicente@vichoreyes.cl>
2024-05-01 23:52:34 -0400

    deindent convertToExcel

--- web_adapter/main.go
+++ web_adapter/main.go
@@ -10,29 +10,31 @@ import (
 
 func main() {
 	keepAlive := make(chan bool)
-	convertToExcel := js.FuncOf(func(this js.Value, p []js.Value) any {
-		if len(p) != 1 {
-			fmt.Println("convertToExcel: expected 1 argument")
-			return nil
-		}
-		jsImageBytes := p[0]
-		byteLength := jsImageBytes.Get("byteLength").Int()
-		buffer := make([]byte, byteLength)
-		byteLengthCopied := js.CopyBytesToGo(buffer, jsImageBytes)
-		if byteLength != byteLengthCopied {
-			fmt.Printf("convertToExcel: expected to copy %d bytes, but got %d\n", byteLength, byteLengthCopied)
-			return nil
-		}
-		fmt.Println("convertToExcel: converting image to excel")
-		dest := new(bytes.Buffer)
-		err := converter.Convert(bytes.NewReader(buffer), dest)
-		if err != nil {
-			fmt.Printf("convertToExcel converter: %v\n", err)
-			return nil
-		}
-		return nil
-	})
+	convertToExcel := js.FuncOf(convertToExcel)
 	js.Global().Set("convertToExcel", convertToExcel)
 	fmt.Println("WebAssembly initialized") // not printing for some reason
 	<-keepAlive
+}
+
+func convertToExcel(this js.Value, p []js.Value) any {
+	if len(p) != 1 {
+		fmt.Println("convertToExcel: expected 1 argument")
+		return nil
+	}
+	jsImageBytes := p[0]
+	byteLength := jsImageBytes.Get("byteLength").Int()
+	buffer := make([]byte, byteLength)
+	byteLengthCopied := js.CopyBytesToGo(buffer, jsImageBytes)
+	if byteLength != byteLengthCopied {
+		fmt.Printf("convertToExcel: expected to copy %d bytes, but got %d\n", byteLength, byteLengthCopied)
+		return nil
+	}
+	fmt.Println("convertToExcel: converting image to excel")
+	dest := new(bytes.Buffer)
+	err := converter.Convert(bytes.NewReader(buffer), dest)
+	if err != nil {
+		fmt.Printf("convertToExcel converter: %v\n", err)
+		return nil
+	}
+	return nil
 }
