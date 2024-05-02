9f70849d3dea9892f41018164536f27532324d78
Vicente Reyes <vicente@vichoreyes.cl>
2024-05-01 22:09:22 -0400

    make html <-> wasm connection work

--- web_adapter/main.go
+++ web_adapter/main.go
@@ -1,1 +1,13 @@
 package main
+
+import "syscall/js"
+
+func main() {
+	keepAlive := make(chan bool)
+	helloWorld := js.FuncOf(func(this js.Value, p []js.Value) interface{} {
+		js.Global().Get("document").Call("getElementById", "output").Set("textContent", "Hello, WebAssembly!")
+		return nil
+	})
+	js.Global().Set("helloWorld", helloWorld)
+	<-keepAlive
+}
