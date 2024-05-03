8301cbb3796542f5a35cb6dda5e8442ff68630b6
Vicente Reyes <vicente@vichoreyes.cl>
2024-05-02 20:38:55 -0400

    shrink images automatically

--- converter/converter.go
+++ converter/converter.go
@@ -12,6 +12,7 @@ import (
 )
 
 const LettersInAlphabet = 26
+const maxImageWidth = 120
 
 // Converts zero-indexed coordinates to letter-and-number excel ones
 func excelIndex(x, y int) string {
@@ -40,12 +41,14 @@ func Convert(imageReader io.Reader, excelWriter io.Writer) error {
 	}
 	log.Printf("Decoded image format: %s", formatName)
 
-	matrix := matrixFromImage(img)
-	saveAsExcel(matrix, excelWriter)
+	bigPxMatrix := matrixFromImage(img)
+	smallerPxMatrix := limitWidth(bigPxMatrix)
+	numberMatrix := spreadY(smallerPxMatrix)
+	saveAsExcel(numberMatrix, excelWriter)
 	return nil
 }
 
-func saveAsExcel(matrix [][]uint32, writer io.Writer) {
+func saveAsExcel(matrix [][]uint8, writer io.Writer) {
 	const sheet = "Sheet1"
 	f := excelize.NewFile()
 	for x, col := range matrix {
@@ -74,20 +77,66 @@ func saveAsExcel(matrix [][]uint32, writer io.Writer) {
 	}
 }
 
-func matrixFromImage(image image.Image) [][]uint32 {
+// turns each pixel into a [r, g, b] array
+func matrixFromImage(image image.Image) [][][3]uint8 {
 	size := image.Bounds().Size()
-	matrix := make([][]uint32, size.X)
+	matrix := make([][][3]uint8, size.X)
 	for i := range matrix {
-		matrix[i] = make([]uint32, size.Y*3)
+		matrix[i] = make([][3]uint8, size.Y)
 	}
 	for x, column := range matrix {
-		for y := 0; y < len(column); y += 3 {
+		for y := 0; y < len(column); y++ {
 			pixelColor := image.At(x, y/3)
 			r, g, b, _ := pixelColor.RGBA()
-			column[y] = r / 0xff
-			column[y+1] = g / 0xff
-			column[y+2] = b / 0xff
+			column[y][0] = uint8(r / 0xff)
+			column[y][1] = uint8(g / 0xff)
+			column[y][2] = uint8(b / 0xff)
 		}
 	}
 	return matrix
+}
+
+// turns each [r, g, b] array into three rows
+func spreadY(imgMatrix [][][3]uint8) [][]uint8 {
+	result := make([][]uint8, len(imgMatrix))
+	for i := range result {
+		result[i] = make([]uint8, len(imgMatrix[i])*3)
+	}
+	for x, column := range imgMatrix {
+		for y, pixel := range column {
+			result[x][y*3] = pixel[0]
+			result[x][y*3+1] = pixel[1]
+			result[x][y*3+2] = pixel[2]
+		}
+	}
+	return result
+}
+
+// makes matrix maxImageWidth pixels wide at most
+func limitWidth(matrix [][][3]uint8) [][][3]uint8 {
+	// round up of len(matrix) / maxImageWidth
+	stride := (len(matrix) + maxImageWidth - 1) / maxImageWidth
+
+	// averages each (stride x stride) square into a single pixel
+	averageOf := func(x, y int) [3]uint8 {
+		var r, g, b uint64
+		for i := x; i < x+stride; i++ {
+			for j := y; j < y+stride; j++ {
+				r += uint64(matrix[i][j][0])
+				g += uint64(matrix[i][j][1])
+				b += uint64(matrix[i][j][2])
+			}
+		}
+		area := uint64(stride * stride)
+		return [3]uint8{uint8(r / area), uint8(g / area), uint8(b / area)}
+	}
+
+	result := make([][][3]uint8, len(matrix)/stride)
+	for i := range result {
+		result[i] = make([][3]uint8, len(matrix[0])/stride)
+		for j := range result[i] {
+			result[i][j] = averageOf(i*stride, j*stride)
+		}
+	}
+	return result
 }
